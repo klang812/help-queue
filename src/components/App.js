db45651ef11c37d00f200c6846588d02473a36db
Kerry Lang <klang812@gmail.com>
2021-04-19 09:42:39 -0700

    set up project

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList";
+
+
+function App(){
+  const name = "Thato";
+  const name2 = "Haley";
+  return (
+    <React.Fragment>
+      <Header />
+      <TicketList />
+    </React.Fragment>
+  );
+}
+
+export default App;
