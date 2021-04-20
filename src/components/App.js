0224427b36d83ea16b6fb18f504429af21cbaf47
Kerry Lang <klang812@gmail.com>
2021-04-20 08:39:48 -0700

    add local state)

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import TicketList from "./TicketList";
+import TicketControl from "./TicketControl";
 
 
 function App(){
@@ -9,7 +9,7 @@ function App(){
   return (
     <React.Fragment>
       <Header />
-      <TicketList />
+      <TicketControl />
     </React.Fragment>
   );
 }
